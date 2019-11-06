d44395fb0e989d62de665f8e72f65ce443ac153d
Michael Matiushyn <mishamatushin@gmail.com>
2019-11-06 23:19:39 +0200

    added js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let a = 7;
+let b = 6;
+let result = a*b;
+
+console.log('Result is ', result);
+
+const isComing = confirm('Please confirm the hotel reservation');
+console.log(isComing);
+console.log(typeof isComing);
+
+let hotelName = prompt('Please enter the name of desired Hotel');
+console.log(hotelName);
+console.log(typeof hotelName);
