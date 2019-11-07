205592226983f70410611f3cf2216aa13d89324b
Michael Matiushyn <mishamatushin@gmail.com>
2019-11-07 22:41:23 +0200

    temp saving

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 'use strict';
 
-let a = 7;
-let b = 6;
-let result = a*b;
+// let a = 7;
+// let b = 6;
+// let result = a*b;
 
-console.log('Result is ', result);
+// console.log('Result is ', result);
 
-const isComing = confirm('Please confirm the hotel reservation');
-console.log(isComing);
-console.log(typeof isComing);
+// const isComing = confirm('Please confirm the hotel reservation');
+// console.log(isComing);
+// console.log(typeof isComing);
 
-let hotelName = prompt('Please enter the name of desired Hotel');
-console.log(hotelName);
-console.log(typeof hotelName);
+// let hotelName = prompt('Please enter the name of desired Hotel');
+// console.log(hotelName);
+// console.log(typeof hotelName);
+
+console.log(Number.parseInt(Math.random()*100));
+console.log(Number.parseInt(Math.random() * 100));
+console.log(Number.parseInt(Math.random() * 100));
+console.log(Number.parseInt(Math.random() * 100));
