67bcfe61ffee547e4995e6cc6320d3ee30a45a1a
Michael Matiushyn <mishamatushin@gmail.com>
2019-11-11 23:22:09 +0200

    added 11.11.19

--- js/index.js
+++ js/index.js
@@ -19,7 +19,90 @@
 // console.log(Number.parseInt(Math.random() * 100));
 // console.log(Number.parseInt(Math.random() * 100));
 
-let month = 'January'
+// let month = 'January'
 
-let season = month === 'January' ? 'summer' : 'winter';
-console.log(season);
+// let season = month === 'January' ? 'summer' : 'winter';
+// console.log(season);
+
+//===========================================================//
+
+// const monthName = "January";
+// let season;
+
+// switch (monthName) {
+//   case 'June':
+//     season = 'summer';
+//     break;
+//   case 'May':
+//     season = 'spring';
+//     break;
+//   case 'September':
+//     season = 'autumn';
+//     break;
+//   default:
+//     season = 'winter';
+// }
+
+// console.log(season);
+
+// ====================================================================
+
+//  const userAge = prompt('Введите Ваш возраст', 18);
+//  console.log(userAge);
+//  console.log(typeof userAge);
+
+// ===========================================================================
+
+// const isGoing = confirm('Вы хотите поехать на шашлык?');
+// console.log(isGoing);
+// console.log(typeof isGoing);
+
+// const str = Number('5');
+// console.log(typeof str);
+
+// const age = Number(prompt('get age', '18'));
+// console.log(typeof age);
+
+// const margin = '15px';
+// console.log(Number.parseInt(margin));
+// const padding = '15.4px';
+// console.log(Number.parseInt(padding));
+// console.log(Number.parseFloat(padding));
+
+// //
+// console.log(Number.parseInt(Math.random() * 10));
+
+// console.log(Math.round(1.5));
+
+// console.log(Math.floor(1.9));
+// console.log(Math.ceil(1.2));
+// console.log(Math.pow(2,3));
+
+// const message = 'I\'m happy!';
+// console.log(message);
+
+// const name = 'Mango';
+// const mood = 'happy';
+
+// const message = name + ' is '+ mood;
+// console.log(message );
+
+// const  message = 'Welcome to Our Resort';
+// console.log('Length of message is ', message.length, 'symbol');
+
+// console.log(message.toLowerCase());
+// console.log(message);
+
+
+// const message = 'Welcome to Uman';
+// console.log(message.includes('to'));
+// console.log(message.includes('Paris'));
+// console.log(message.indexOf('to'));
+
+// const name = 'Mango';
+// const age = 5;
+// const mood = 'happy';
+
+// const message = `My name is ${name}, I'm ${age} years old and very ${mood}.`;
+// console.log(message);
+
