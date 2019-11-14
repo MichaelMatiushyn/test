b79319afaf56642c1d8cfee8164f9221254ca8cf
Michael Matiushyn <mishamatushin@gmail.com>
2019-11-15 00:45:01 +0200

    finished module 1

--- js/index.js
+++ js/index.js
@@ -113,10 +113,106 @@
 
 // console.log(isInRange);
 
-const num = Number(prompt('Введите пожалуйста число до 10'));
+// const num = Number(prompt('Введите пожалуйста число до 10'));
 
-if (num <= 10) {
-  console.log('Отлично все подходит');
+// // if (num >= 10 && num <= 30) {
+// //   console.log('Отлично все подходит');
+// // } else {
+// //   console.log('Все плохо! число не подходит');
+// // }
+// //  ===================================== или
+
+// const isInRange = num >= 10 && num <= 30;
+
+// if (isInRange) {
+//   console.log('все супер');
+// } else {
+//   console.log('fuck');
+// }
+
+// Продаем кофе, разные размеры стаканы
+// const SIZE_SMALL = 0;
+// const SIZE_MEDIUM = 1;
+// const SIZE_LARGE = 2;
+
+// const input = Number(prompt('0-small, 1- medium, 2- large', 0));
+
+// if (input === SIZE_SMALL) {
+//   console.log('готовим маленький кофе');
+// } else if (input === SIZE_MEDIUM) {
+//   console.log('готовим средний кофе');
+// } else if (input === SIZE_LARGE) {
+//   console.log('готовим большой кофе');
+// } else {
+//   console.log('Ошибка');
+// }
+
+// Попросить у пользователя число, если больше 10, записать в message "ура больше 10", в противном случае все плохо
+
+// const input = Number(prompt('введите число больше 10'));
+// // let message;
+
+// // if (input > 10) {
+// //   message = 'ура, больше 10!';
+// // } else {
+// //   message = 'все плохо';
+// // }
+// // alert(message);
+
+// let message = input >=10 ? 'ура больше 10' : 'все плохо!';
+// alert(message);
+
+const SIZE_SMALL = 0;
+const SIZE_MEDIUM = 1;
+const SIZE_LARGE = 2;
+
+const PRICE_SMALL = 50;
+const PRICE_MEDIUM = 100;
+const PRICE_LARGE = 150;
+
+const userInput = prompt('0-small, 1- medium, 2- large', 0);
+const coffeeSize = Number(userInput);
+const isValidInput = userInput !== null && !Number.isNaN(coffeeSize);
+
+console.log(isValidInput);
+if (isValidInput) {
+  let haveCoffee;
+  let size;
+  let price;
+
+  switch (coffeeSize) {
+    case SIZE_SMALL:
+      haveCoffee = true;
+      size = 'small';
+      price = PRICE_SMALL;
+      break;
+
+    case SIZE_MEDIUM:
+      haveCoffee = true;
+      size = 'medium';
+      price = PRICE_MEDIUM;
+      break;
+
+    case SIZE_LARGE:
+      haveCoffee = true;
+      size = 'large';
+      price = PRICE_LARGE;
+      break;
+
+    default:
+      console.log('такого кофе нет');
+      haveCoffee = false;
+  }
+
+  if (haveCoffee) {
+    const userCash = Number(prompt(`Заказан ${size}, с тебя ${price}`));
+    if (userCash >= price) {
+      let change = userCash - price;
+      alert(`Вот ваш ${size} кофе. Ваша сдача составила ${change} гривен`);
+    } else {
+      alert('А денег то не хватило!');
+    }
+  }
 } else {
-  console.log('Все плохо! число не подходит');
+  alert('Ввод не валидный!');
 }
