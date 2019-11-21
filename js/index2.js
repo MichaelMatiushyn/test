a8703c65828238205406934b6ee2f108353f41b3
Michael Matiushyn <mishamatushin@gmail.com>
2019-11-21 23:25:00 +0200

    started module 2

---
+++ js/index2.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Модуль 2. Массивы и циклы
+
+// const clients = ['Mango', 'Poly', 'Ajax'];
+// console.log(clients[0]);
+// console.log(clients[1]);
+
+// clients[0] = 'Chelsey';
+
+// console.log(clients);
+// clients[3] = 'Alex';
+// console.log(clients);
+// console.log(clients.length);
+// console.log(typeof clients.length);
+
+// const message = "Welcome to Bahamas!"
+// console.log(message.split(" "));
+
+// const number = 10;
+// for (let i = 0; i < number; i += 1) {
+//   console.log(i);
+// }
+
+// for (let i = 0, max = 10; i < max; i += 1) {
+//   console.log(`${max}%${i}`, max % i);
+// }
+// const clients = ['Mango', 'Ajax', 'Poly'];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(typeof clients[i]);
+// }
+// const numbers = [];
+// for (let i = 0; i < 10; i += 1) {
+//   numbers.push(`number-${i}`);
+// }
+// console.log(numbers);
