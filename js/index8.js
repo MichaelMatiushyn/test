032541e6834c6425198a177d732dbe9c5b62b1d9
Michael Matiushyn <mishamatushin@gmail.com>
2020-06-10 23:56:29 +0300

    added8.2

--- js/index8.js
+++ js/index8.js
@@ -177,22 +177,239 @@
   При клике по ссылкам не должна перезагружаться страница!
 */
 
-const menu = $qs('.js-menu');
-const links = menu.querySelectorAll('.menu-link');
-menu.addEventListener('click', handleMenuClick);
+// const menu = $qs('.js-menu');
+// const links = menu.querySelectorAll('.menu-link');
+// menu.addEventListener('click', handleMenuClick);
 
-function handleMenuClick({ target }) {
-  const nodeName = target.nodeName;
-  event.preventDefault();
-  setActiveLink(links, target);
+// function handleMenuClick({ target }) {
+//   const nodeName = target.nodeName;
+//   event.preventDefault();
+//   setActiveLink(links, target);
+// }
+
+// function setActiveLink(links, target) {
+//   links.forEach(link => {
+//     if (link !== target) {
+//       link.classList.remove('active');
+//     } else {
+//       link.classList.add('active');
+//     }
+//   });
+// }
+
+// Обработчик местоположения мышки
+// ((doc, win) => {
+//   const input = doc.querySelector('.js-input');
+//   // input.addEventListener('focusout', e => console.log(e));
+//   // input.addEventListener('focus', e => console.log(e));
+//   // input.focus();
+//   // input.addEventListener('change', e => console.log(e.target.value));
+
+//   // с 2018
+//   input.addEventListener('input', e => console.log(e.target.value));
+
+//   const winCoordsE1 = doc.querySelector('.js-win-coords');
+//   const docCoordsE1 = doc.querySelector('.js-doc-coords');
+//   updateCoords(0, 0, 0, 0);
+
+//   insertSpacer();
+
+//   win.addEventListener('mousemove', handleMouseMove);
+
+//   // Для отслеживания только клавиш, которые выводят символ
+//   // win.addEventListener('keypress', e => {
+//   //   console.log(e);
+//   // });
+//   //
+//   // win.addEventListener('keydown', e => {
+//   //   console.log(e);
+//   // });
+
+//   function handleMouseMove({ clientX, clientY, pageX, pageY }) {
+//     updateCoords(clientX, clientY, pageX, pageY);
+//   }
+//   function updateCoords(cx, cy, px, py) {
+//     winCoordsE1.textContent = `Относительно окна: (clientX: ${cx}, clientY: ${cy}) `;
+//     docCoordsE1.textContent = `Относительно документа: (pageX: ${px}, pageY: ${py})`;
+//   }
+//   function insertSpacer() {
+//     const spacer = doc.createElement('div');
+//     spacer.style.height = '2000px';
+//     doc.body.appendChild(spacer);
+//   }
+// })(document, window);
+
+// ((w, d) => {
+//   const chattyValue = d.querySelector('.chatty-container');
+//   let counter = 0;
+
+//   w.addEventListener('mousemove', throttle(handleMouseMove, 200));
+
+//   // w.addEventListener('mousemove', throttleWithRAF(handleMouseMove));
+
+//   function handleMouseMove(event) {
+//     counter += 1;
+//     chattyValue.textContent = counter;
+//   }
+
+//   function throttle(fn, delay) {
+//     let lastCall = 0;
+
+//     return function (...args) {
+//       const now = new Date().getTime();
+//       if (now - lastCall < delay) {
+//         return;
+//       }
+//       lastCall = now;
+
+//       return fn(...args);
+//     };
+//   }
+//   function throttleWithRAF(fn) {
+//     let isFiring = false;
+
+//     return function (...args) {
+//       if (!isFiring) {
+//         requestAnimationFrame(() => {
+//           fn(...args);
+//           isFiring = false;
+//         });
+//       }
+//       isFiring = true;
+//     };
+//   }
+// })(window, document);
+
+// Для скрола
+// ((win, doc) => {
+//   const offsetE1 = doc.querySelector('.js-page-y-offset');
+//   const paragraphs = Array.from(doc.querySelectorAll('.js-text'));
+//   const offsets = collectVerticalOffsets(paragraphs);
+//   // const unique = doc.querySelector('.unique');
+
+//   // console.log('offsetWidth: ', offsetE1.offsetWidth);
+//   // console.log('offsetHeight: ', offsetE1.offsetHeight);
+
+//   // const coordinates = unique.getBoundingClientRect();
+//   // console.log(coordinates);
+//   updateOffsetValue(0);
+
+//   win.addEventListener('scroll', handleScroll);
+
+//   function handleScroll(event) {
+//     updateOffsetValue(pageYOffset);
+//     setActiveParagraph(pageYOffset);
+//   }
+
+//   function setActiveParagraph(currentWindowScroll) {
+//     Object.keys(offsets).forEach(key => {
+//       if (offsets[key] <= currentWindowScroll) {
+//         setActiveItem(paragraphs, key);
+//       }
+//     });
+//   }
+
+//   function setActiveItem(items, idx) {
+//     const prevActiveItem = doc.querySelector('.js-text.current');
+
+//     if (prevActiveItem) {
+//       prevActiveItem.classList.remove('current');
+//     }
+//     const currActiveItem = items[idx];
+//     currActiveItem.classList.add('current');
+//   }
+
+//   function collectVerticalOffsets(elements) {
+//     return elements.reduce((offsets, el, idx) => {
+//       offsets[idx] = el.offsetTop;
+//       return offsets;
+//     }, {});
+//   }
+
+//   function updateOffsetValue(val) {
+//     offsetE1.textContent = `pageYOffset: ${Math.round(val)}`;
+//   }
+// })(window, document);
+
+// ============= Пример использования =======
+
+// const nav = document.querySelector('.nav');
+// const list = document.querySelector('.nav > ul');
+// const toTopLink = document.querySelector('.to-top-link');
+// const navCoords = nav.getBoundingClientRect();
+
+// window.addEventListener('scroll', function () {
+//   if (pageYOffset >= navCoords.top) {
+//     document.body.style.paddingTop = nav.offsetHeight + 'px';
+//     nav.classList.add('nav-fixed');
+//   } else {
+//     document.body.style.paddingTop = '';
+//     nav.classList.remove('nav-fixed');
+//   }
+// });
+
+// list.addEventListener('click', function (evt) {
+//   const target = evt.target;
+
+//   if (target === this || target.nodeName === 'LI') return;
+
+//   evt.preventDefault();
+
+//   const href = target.getAttribute('href');
+//   const el = document.querySelector(href);
+
+//   const top =
+//     el.getBoundingClientRect().top + window.pageYOffset - nav.offsetHeight;
+
+//   window.scrollTo({ top: top, behavior: 'smooth' });
+// });
+
+// toTopLink.addEventListener('click', function (evt) {
+//   evt.preventDefault();
+
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+// });
+
+// ===========================================
+
+const sections = $qsa('.section');
+const navContainerHeight = $qs('.page-header').offsetHeight;
+
+const verticalOffsets = collectVerticalOffsets(sections);
+setTopBodyPadding(navContainerHeight);
+
+window.addEventListener(
+  'scroll',
+  handleWindowScroll.bind(null, verticalOffsets),
+);
+
+function setTopBodyPadding(val) {
+  document.body.style.paddingTop = val + 'px';
 }
 
-function setActiveLink(links, target) {
-  links.forEach(link => {
-    if (link !== target) {
-      link.classList.remove('active');
-    } else {
-      link.classList.add('active');
+function collectVerticalOffsets(elements) {
+  const obj = {};
+
+  elements.forEach(el => {
+    obj[el.id] = el.offsetTop;
+  });
+  return obj;
+}
+
+function handleWindowScroll(offsets) {
+  const scrollPos =
+    document.documentElement.scrollTop || document.body.scrollTop;
+
+  Object.keys(offsets).forEach(key => {
+    if (offsets[key] <= scrollPos) {
+      setActiveItem(key);
     }
   });
+}
+function setActiveItem(id) {
+  const prevActiveItem = $qs('.site-nav a.active');
+  prevActiveItem.classList.remove('active');
+
+  const curActiveItem = $qs(`.site-nav a[href*='${id}']`);
+  curActiveItem.classList.add('active');
 }
