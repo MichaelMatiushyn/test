f98b5ea210b044e91ae7983a417dbe5db3bdb712
Michael Matiushyn <mishamatushin@gmail.com>
2020-08-31 22:26:23 +0300

    added module 12 begin

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-const shortId = require('shortid');
-const axios = require('axios');
+// const shortId = require('shortid');
+// const axios = require('axios');
 
-// import { logUpperCase, logLowerCase } from './logger';
-import * as loggerModule from './logger';
+// // import { logUpperCase, logLowerCase } from './logger';
+// import * as loggerModule from './logger';
 
-require('./styles.css');
+// require('./styles.css');
+
+// преобразование в ES6
+import shortId from 'shortId';
+import axios from 'axios';
+import * as logger from './logger';
+import './styles.css';
 
 axios
   .get('https://jsonplaceholder.typicode.com/posts/1')
@@ -14,5 +20,9 @@ console.log(shortId());
 
 // logUpperCase('qweqwe message upper!!!!');
 // logLowerCase('CHECK these letters WITH small');
-loggerModule.logLowerCase('CHECK THIS MAN ');
-loggerModule.logUpperCase('from small letters');
+// --------------------------------------------------------
+// loggerModule.logLowerCase('CHECK THIS MAN ');
+// loggerModule.logUpperCase('from small letters');
+// --------------------------------------------------
+logger.logLowerCase('CHECK THIS NEW ATTEMPT');
+logger.logUpperCase('new att with small letters');
